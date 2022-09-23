eb5f97fb936660cea0dedd1755785dae20969a74
Zenzok30 <clemencekumz@gmail.com>
2022-09-23 22:38:14 +0200

    Fix code

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line,to stdout
+ * @str: String to be printed out
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
